03c28dcd48b90e06a805edfad17b1e2579c09447
Erik Sanne <erik@eriksanne.com>
2021-01-25 00:05:43 +0100

    Add profile avatar and menu to navbar

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -2,24 +2,38 @@
   <div>
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Dashy</v-toolbar-title>
+      <v-app-bar-title>Dashy</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <AccountMenu></AccountMenu>
     </v-app-bar>
     <v-navigation-drawer
         v-model="drawer"
         absolute
         temporary
     >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Dashy
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
       <v-list nav dense>
         <v-list-item-group
             v-model="group"
-            active-class="deep-purple--text"
+            :active-class="$vuetify.theme.dark ? 'light-blue--text' : 'deep-purple--text'"
         >
-          <v-list-item>
+          <v-list-item
+              v-for="(item, i) in $store.state.navLinks"
+              :key="i"
+              :to="item.route"
+          >
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>{{item.icon}}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>
+              {{item.name}}</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -28,8 +42,10 @@
 </template>
 
 <script>
+import AccountMenu from "@/components/nav/NavMenu";
 export default {
   name: "NavBar",
+  components: {AccountMenu},
   data: () => ({
     drawer: false,
     group: null,
---
+++ src/components/nav/NavMenu.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-menu
+      bottom
+      min-width="200px"
+      rounded
+      offset-y
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn large icon class="mr-2" v-on="on">
+        <v-avatar color="secondary">ES</v-avatar>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-list-item-content class="justify-center pb-0">
+        <div class="mx-auto text-center">
+          <h3>{{ user.fullName }}</h3>
+          <p class="caption mt-1 mb-1">
+            {{ user.email }}
+          </p>
+          <v-divider></v-divider>
+          <v-btn
+            block
+            text
+          >
+            Profile
+          </v-btn>
+          <v-divider></v-divider>
+          <v-btn
+              block
+              text
+          >
+            Settings
+          </v-btn>
+          <v-divider></v-divider>
+          <v-btn
+              block
+              text
+          >
+            Log out
+          </v-btn>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "AccountMenu",
+  data: () => ({
+    user: {
+      initials: 'JD',
+      fullName: 'John Doe',
+      email: 'john.doe@doe.com',
+    }
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
