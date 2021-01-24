f1ffb2a38cdd661b4a16178905e97df56cbe2a14
Erik Sanne <erik@eriksanne.com>
2021-01-25 00:06:33 +0100

    Add auth forms

---
+++ src/components/auth/LoginForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card>
+    <v-card-title>Login</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-row>
+          <v-col
+              cols="12"
+          >
+            <v-text-field
+                v-model="username"
+                :rules="usernameRules"
+                label="Username"
+                required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+              cols="12"
+          >
+            <v-text-field
+                v-model="password"
+                :rules="passwordRules"
+                label="Password"
+                required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+              cols="12"
+          >
+            <v-btn block>
+              Login
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+name: "LoginForm"
+}
+</script>
+
+<style scoped>
+
+</style>
